6c43237106e7e4df51b6420140bcea629c08ccbe
malavbhavsar <malav.bhavsar@gmail.com>
2012-11-02 18:45:45 -0400

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbhack::Application.config.secret_token = '06bfc4c40028e35a701c8d4158917b94d3223060b6a2d4846713de7f585e359aeca153f5f3df1123a7a7f33f19026e59016d15b0d846a64ff2b5e03382bc82cb'
